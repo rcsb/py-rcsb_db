5e6e792e665d91dca87b6246537628ea51c29e9c
jwest <john.westbrook@rcsb.org>
2018-12-13 15:45:01 -0500

    V0.35 Add ihm_dev collection and support for I/HM repository model file  processing (BACK-155).

--- rcsb/db/tests-mongo/TEST-REPO-LOAD-EXEC-MOCK.sh
+++ rcsb/db/tests-mongo/TEST-REPO-LOAD-EXEC-MOCK.sh
@@ -3,6 +3,7 @@
 # Date: 3-Jul-2018 jdw
 #
 # Example mock repository load (MongoDb)
+#
 exdb_repo_load_cli  --mock --full  --load_chem_comp_ref       --working_path ./test-output  --config_path ../../mock-data/config/dbload-setup-example.yml --config_name site_info --fail_file_list_path ./test-output/failed-cc-path-list.txt --read_back_check >& ./test-output/LOGCHEMCOMPFULL
 exdb_repo_load_cli  --mock --full  --load_chem_comp_core_ref  --working_path ./test-output  --config_path ../../mock-data/config/dbload-setup-example.yml --config_name site_info --fail_file_list_path ./test-output/failed-cc-core-path-list.txt --read_back_check >& ./test-output/LOGCHEMCOMPCOREFULL
 exdb_repo_load_cli  --mock --full  --load_bird_chem_comp_ref  --working_path ./test-output  --config_path ../../mock-data/config/dbload-setup-example.yml --config_name site_info --fail_file_list_path ./test-output/failed-bird-cc-path-list.txt --read_back_check >& ./test-output/LOGBIRDCHEMCOMPFULL
@@ -10,4 +11,5 @@ exdb_repo_load_cli  --mock --full  --load_bird_ref            --working_path ./t
 exdb_repo_load_cli  --mock --full  --load_bird_family_ref     --working_path ./test-output  --config_path ../../mock-data/config/dbload-setup-example.yml --config_name site_info --fail_file_list_path ./test-output/failed-family-path-list.txt --read_back_check >& ./test-output/LOGBIRDFAMILY
 exdb_repo_load_cli  --mock --full  --load_entry_data          --working_path ./test-output  --config_path ../../mock-data/config/dbload-setup-example.yml --config_name site_info --save_file_list_path ./test-output/LATEST_PDBX_LOAD_LIST.txt --fail_file_list_path ./test-output/failed-entry-path-list.txt >& ./test-output/LOGENTRYFULL
 exdb_repo_load_cli  --mock --full  --load_pdbx_core           --working_path ./test-output  --config_path ../../mock-data/config/dbload-setup-example.yml --config_name site_info --save_file_list_path ./test-output/LATEST_PDBX_CORE_LOAD_LIST.txt --fail_file_list_path ./test-output/failed-pdbx-core-path-list.txt >& ./test-output/LOGPDBXCOREFULL
+exdb_repo_load_cli  --mock --full  --load_ihm_dev             --working_path ./test-output  --config_path ../../mock-data/config/dbload-setup-example.yml --config_name site_info --save_file_list_path ./test-output/LATEST_IHM_DEV_LOAD_LIST.txt --fail_file_list_path ./test-output/failed-ihm-dev-path-list.txt >& ./test-output/LOGIHMDEVFULL
 #
