bda603449d312e4bcef41bb7b3555e4d7b2b70f3
jwest <john.westbrook@rcsb.org>
2021-01-16 10:41:34 -0500

    V1.656 Further enumeration updates for polymer types.

--- rcsb/db/tests-validate/testSchemaDataPrepValidate.py
+++ rcsb/db/tests-validate/testSchemaDataPrepValidate.py
@@ -78,7 +78,7 @@ class SchemaDataPrepValidateTests(unittest.TestCase):
         self.__modulePathMap = self.__cfgOb.get("DICT_METHOD_HELPER_MODULE_PATH_MAP", sectionName=configName)
         self.__testDirPath = os.path.join(HERE, "test-output", "pdbx-fails")
         self.__testIhmDirPath = os.path.join(HERE, "test-output", "ihm-files")
-        self.__export = False
+        self.__export = True
         #
         self.__extraOpts = None
         # The following for extended parent/child info -
