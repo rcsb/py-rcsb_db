c64745f593e334a0e2b7eda39aee70ad393ab570
Michael Trumbull <michael.trumbull@rcsb.org>
2025-01-22 14:29:12 -0500

    adding initial tests

---
+++ rcsb/db/tests/testPdbCsmImageWorkflow.py
@@ -0,0 +1,120 @@
+##
+# File:    testPdbCsmImageWorkflow.py
+# Author:  Michael Trumbull
+# Date:    12-Dec-2024
+# Version: 0.01
+#
+# Updates:
+#
+##
+
+__docformat__ = "google en"
+__author__ = "Michael Trumbull"
+__email__ = "michael.trumbull@rcsb.org"
+__license__ = "Apache 2.0"
+__version__ = "V0.01"
+
+import logging
+import os
+import platform
+import resource
+import time
+import unittest
+from pathlib import Path
+
+from rcsb.db.wf.RepoLoadWorkflow import RepoLoadWorkflow
+
+#
+# Warning: This does not test jpg generation only idList generation.
+# This is because infrastructure would need to accomodate node.js
+# which is excessive.
+#
+
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+TOPDIR = os.path.dirname(os.path.dirname(os.path.dirname(HERE)))
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s]-%(module)s.%(funcName)s: %(message)s")
+logger = logging.getLogger(__name__)
+
+
+class TestPdbCsmImageWorkflow(unittest.TestCase):
+    def setUp(self) -> None:
+        self.__startTime = time.time()
+        # self.__cachePath = os.path.join(HERE, "test-data")
+        self.__workPath = os.path.join(HERE, "test-output", "CACHE")
+        self.mockdataDir = os.path.join(TOPDIR, "rcsb", "mock-data", "MOCK_IMGS_WF_BCIF_DATA")
+        logger.debug("Running tests on version %s", __version__)
+        logger.info("Starting %s at %s", self.id(), time.strftime("%Y %m %d %H:%M:%S", time.localtime()))
+
+    def tearDown(self) -> None:
+        unitS = "MB" if platform.system() == "Darwin" else "GB"
+        rusageMax = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+        logger.info("Maximum resident memory size %.4f %s", rusageMax / 10 ** 6, unitS)
+        endTime = time.time()
+        logger.info("Completed %s at %s (%.4f seconds)", self.id(), time.strftime("%Y %m %d %H:%M:%S", time.localtime()), endTime - self.__startTime)
+
+    def testIdListGeneration(self) -> None:
+        """Test id list file generation ..."""
+        try:
+            logger.info('creating object')
+            pciWF = PdbCsmImageWorkflow()
+            logger.info("Generating 3 id lists to run through.")
+            logger.info('mockdataDir %s', self.mockdataDir)
+            logger.info('workpath %s', self.__workPath)
+            pciWF.imagesGenLists(
+                pdbGzPath=os.path.join(self.mockdataDir, "released_structures_last_modified_dates.json.gz"),
+                updateAllImages=True,
+                pdbBaseDir=self.mockdataDir,
+                csmGzPath=os.path.join(self.mockdataDir, "computed-models-holdings.json.gz"),
+                csmBaseDir=self.mockdataDir,
+                numWorkers=3,
+                idListPath=self.__workPath,
+            )
+
+            logger.info("Reading generated lists and checking for format.")
+
+            def checkList(ids: str) -> bool:
+                try:
+                    logger.info('ids path for checkList %s', ids)
+                    allDataPresent = True
+                    with Path(ids).open("r", encoding="utf-8") as file:
+                        idList = [line.rstrip("\n") for line in file]
+                    for line in idList:
+                        logger.info('line from file is: %s', line)
+                        fileId, bcifFileName, sdm = line.split(" ")
+                        if not ((len(fileId) > 0) and (len(bcifFileName) > 0) and (len(sdm) > 0)):
+                            logger.error('Found one of the following had a length of zero %s %s %s', fileId, bcifFileName, sdm)
+                            allDataPresent = False
+                    logger.info('End of a single checkList. Returning a value of %s', allDataPresent)
+                    return allDataPresent
+                except Exception:
+                    logger.exception("Failed to find created file %s", ids)
+                    return False
+
+            ok1 = checkList(os.path.join(self.__workPath, "idList_0.txt"))
+            if not ok1:
+                logger.error("idList_0.txt failed")
+            self.assertTrue(ok1)
+            ok2 = checkList(os.path.join(self.__workPath, "idList_1.txt"))
+            if not ok2:
+                logger.error("idList_1.txt failed")
+            self.assertTrue(ok2)
+            ok3 = checkList(os.path.join(self.__workPath, "idList_2.txt"))
+            if not ok3:
+                logger.error("idList_2.txt failed")
+            self.assertTrue(ok3)
+        except Exception as e:
+            logger.exception("Failing with %s", str(e))
+            self.fail("Failed to build idLists")
+
+
+def suiteFileGeneration():
+    suiteSelect = unittest.TestSuite()
+    suiteSelect.addTest(TestPdbCsmImageWorkflow("testIdListGeneration"))
+    return suiteSelect
+
+
+if __name__ == "__main__":
+    mySuite = suiteFileGeneration()
+    unittest.TextTestRunner(verbosity=2).run(mySuite)
